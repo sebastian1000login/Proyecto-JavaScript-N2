a4a65741c45dcf53d134a4606ba8dc4dc9b6aa14
sebastian1000login <sebastian1000login@gmail.com>
2025-11-24 23:56:32 -0500

    Added changes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,78 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const express = require("express");
+const app = express();
+const mysql = required("mysql");
+const cors = require("cors");
+app.use(cors());
+app.use(express.body());
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "dbalmacen"
+});
+
+app.post("/create", (req, res)=>{
+    const nombre = req.body.nombre;
+    const precio = req.body.precio;
+    const stock = req.body.stock;
+    db.query('insert into producto (despro, precio, stock) values (?, ?, ?)', [nombre, precio, stock],
+    (err, result)=>{
+        if (err)
+        {
+            console.log(err);
+        }else
+        {
+            res.send("grabacion ok");
+        }
+    });
+});
+app.post("/update", (req, res)=>{
+    const id = req.body.id;
+    const nombre = req.body.nombre;
+    const precio = req.body.precio;
+    const stock = req.body.stock;
+    db.query('update producto set despro = ?, precio = ?, stock = ? where producto = ?', [nombre, precio, stock, id],
+    (err, result)=>{
+        if (err)
+        {
+            console.log(err);
+        }else
+        {
+            res.send("grabacion ok");
+        }
+    });
+});
+app.post("/productos", (req, res)=>{
+    db.query('select * from producto',
+    (err, result)=>{
+        if (err)
+        {
+            console.log(err);
+        }else
+        {
+            res.send(result);
+        }
+    });
+});
+app.post("/delete/id", (req, res)=>{
+    const id = req.body.id;
+    db.query('delete from productos where codpro = ?', [id],
+    (err, result)=>{
+        if (err)
+        {
+            console.log(err);
+        }else
+        {
+            res.send(result);
+        }
+    });
+});
+app.listen(3001, ()=>{
+    console.log("puerto 3001 escucha");
+});
 root.render(
   <React.StrictMode>
     <App />
